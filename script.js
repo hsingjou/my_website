e5f1817c3914326e1b4f29ab05312a92274b7c96
hsungjou <shero011735@gmail.com>
2023-09-25 11:31:41 +0800

    0925-1

---
+++ script.js
@@ -0,0 +1,43 @@
+$( ".logo" ).textillate();
+
+function ig_exercise_on(){
+    document.getElementById("ig_exercise_overlay").style.display="block";
+}
+function off_ig_exercise(){
+    document.getElementById("ig_exercise_overlay").style.display="none";
+}
+
+function conan_website_on(){
+    document.getElementById("conan_website_overlay").style.display="block";
+}
+function off_conan_website(){
+    document.getElementById("conan_website_overlay").style.display="none";
+}
+
+function list_on(){
+    document.getElementById("list_overlay").style.display="block";
+}
+function off_list(){
+    document.getElementById("list_overlay").style.display="none";
+}
+
+function quiz_on(){
+    document.getElementById("quiz_overlay").style.display="block";
+}
+function off_quiz(){
+    document.getElementById("quiz_overlay").style.display="none";
+}
+
+function weather_search_on(){
+    document.getElementById("weather_search_overlay").style.display="block";
+}
+function off_weather_search(){
+    document.getElementById("weather_search_overlay").style.display="none";
+}
+
+function dessert_shop_on(){
+    document.getElementById("dessert_shop_overlay").style.display="block";
+}
+function off_dessert_shop(){
+    document.getElementById("dessert_shop_overlay").style.display="none";
+}
