832cdc907a63fdb6e6dbebfe5d2314c9b66a64c3
hsungjou <shero011735@gmail.com>
2023-10-02 13:20:27 +0800

    1002-1

--- script.js
+++ script.js
@@ -1,43 +1,28 @@
 $( ".logo" ).textillate();
 
-function ig_exercise_on(){
-    document.getElementById("ig_exercise_overlay").style.display="block";
-}
-function off_ig_exercise(){
-    document.getElementById("ig_exercise_overlay").style.display="none";
+function show_menu_items(){
+    document.getElementById("show_menu_overlay").style.display="block";
 }
 
-function conan_website_on(){
-    document.getElementById("conan_website_overlay").style.display="block";
+function off_menu_overlay(){
+    document.getElementById("show_menu_overlay").style.display="none";
 }
-function off_conan_website(){
-    document.getElementById("conan_website_overlay").style.display="none";
-}
+function scrollToSection(sectionId) {
+    // 滾動到相應的位置
+    const section = document.getElementById(sectionId);
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+    }
 
-function list_on(){
-    document.getElementById("list_overlay").style.display="block";
+    // 關閉overlay
+    const overlay = document.getElementById('show_menu_overlay');
+    if (overlay) {
+        overlay.style.display = 'none';
+    }
 }
-function off_list(){
-    document.getElementById("list_overlay").style.display="none";
-}
-
-function quiz_on(){
-    document.getElementById("quiz_overlay").style.display="block";
-}
-function off_quiz(){
-    document.getElementById("quiz_overlay").style.display="none";
-}
-
-function weather_search_on(){
-    document.getElementById("weather_search_overlay").style.display="block";
-}
-function off_weather_search(){
-    document.getElementById("weather_search_overlay").style.display="none";
-}
-
-function dessert_shop_on(){
-    document.getElementById("dessert_shop_overlay").style.display="block";
-}
-function off_dessert_shop(){
-    document.getElementById("dessert_shop_overlay").style.display="none";
-}
+// 在螢幕大小變化時檢測並自動隱藏overlay
+window.addEventListener('resize', function () {
+    if (window.innerWidth >= 600) {
+        off_menu_overlay();
+    }
+});
